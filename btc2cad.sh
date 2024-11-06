66e7c001ed43172deb2f7716133717491026c8ba
Mathieu Rousseau <mathieu@2027a.net>
2024-11-05 20:51:47 -0500

    show a CAD value notification for a determined amount of BTC

---
+++ btc2cad.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+BTC_AMOUNT=$MY_BTC_AMOUNT
+COINGECKO_API_ENDPOINT="https://api.coingecko.com/api/v3/simple/price"
+CRYPTOCURRENCY="bitcoin"
+CURRENCY="cad"
+
+# Fetching BTC/CAD value from CoinGecko API
+response=$(curl -s "${COINGECKO_API_ENDPOINT}?ids=${CRYPTOCURRENCY}&vs_currencies=${CURRENCY}")
+
+if [ $? -ne 0 ]; then
+    notify-send "Error" "Failed to fetch data"
+    exit 1
+fi
+
+btc_to_cad=$(echo "$response" | jq ".${CRYPTOCURRENCY}.${CURRENCY}")
+
+if [ $? -ne 0 ]; then
+    notify-send "Error" "Failed to parse data"
+    exit 1
+fi
+
+total_value=$(echo "$BTC_AMOUNT * $btc_to_cad" | bc -l)
+
+notify-send "BTC/CAD" "$(printf 'Mes Bitcoins valent %.2f CAD' "$total_value")"
