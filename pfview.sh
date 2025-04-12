b7f1822c111211e565a6358379206e869d3ed934
Mathieu Rousseau <mathieu@2027a.net>
2025-04-12 19:12:29 -0400

    cleaning

--- pfview.sh
+++ pfview.sh
@@ -3,9 +3,8 @@
 result=$(ticker print --config ~/Coffre/portfolio.yaml | jq -r '.[] | "\(.symbol | sub("-CAD"; "")): \((.value | tonumber * 100 | floor) / 100 // 0)"')
 
 if [ $? -ne 0 ]; then
-    notify-send "Error" "Failed to fetch data"
+    notify-send "Error" "Failed to get data"
     exit 1
 fi
 
-LC_NUMERIC=C
 notify-send "Portfolio" "$result"
