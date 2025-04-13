82f4c5e9e68ca6eeca691add6eb4a1a29d52a58c
Mathieu Rousseau <mathieu@2027a.net>
2025-04-13 19:37:16 -0400

    cleaner look

--- keepawake_status.sh
+++ keepawake_status.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ -f /tmp/keepawake.lock ] && kill -0 $(cat /tmp/keepawake.lock) 2>/dev/null; then
-    echo '{"text": "󰄯", "tooltip": "Idle inhibited", "class": "keepawake"}'
+    echo '{"text": "", "tooltip": "Idle inhibited", "class": "keepawake"}'
 else
-    echo '{"text": "󰄰", "tooltip": "Idle not inhibited", "class": "no-keepawake"}'
+    echo '{"text": "", "tooltip": "Idle not inhibited", "class": "no-keepawake"}'
 fi
